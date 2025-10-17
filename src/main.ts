58887622a14b89a859d0a2f9812b11687f66ef23
bfereydo <bfereydouni023@gmail.com>
2025-10-16 19:40:53 -0700

    Added clear function as well as drawing capability. Drawing gets disabled if mouse goes out of canvas. Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,48 @@ import "./style.css";
 const canvas = document.querySelector("canvas")!;
 canvas.width = 256;
 canvas.height = 256;
-//const ctx = canvas.getContext("2d")!;
+const ctx = canvas.getContext("2d")!;
+const clearButton = document.getElementById("clear-button")!;
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+const cursor = { x: 0, y: 0, isDown: false };
+
+canvas.addEventListener("mousedown", (e) => {
+  if (e.button !== 0) {
+    return;
+  }
+  cursor.isDown = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+canvas.addEventListener("mouseup", (e) => {
+  if (e.button !== 0) {
+    return;
+  }
+  cursor.isDown = false;
+  ctx.closePath();
+});
+canvas.addEventListener("mousemove", (e) => {
+  if (!cursor.isDown) {
+    return;
+  }
+  draw(e.offsetX, e.offsetY);
+});
+canvas.addEventListener("mouseleave", (_e) => {
+  cursor.isDown = false;
+  ctx.closePath();
+});
+
+function draw(x: number, y: number) {
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 4;
+  ctx.lineCap = "round";
+  ctx.beginPath();
+  ctx.moveTo(cursor.x, cursor.y);
+  ctx.lineTo(x, y);
+  ctx.stroke();
+  cursor.x = x;
+  cursor.y = y;
+}
